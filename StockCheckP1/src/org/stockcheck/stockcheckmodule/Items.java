1f5970d2f66e5d05e483abc40eb63506bb8290d9
Marius Simk <wertasto@gmail.com>
2017-04-05 20:13:20 +0100

    First commit

---
+++ StockCheckP1/src/org/stockcheck/stockcheckmodule/Items.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.stockcheck.stockcheckmodule;
+
+/**
+ *
+ * @author User
+ */
+public class Items {
+    
+}
